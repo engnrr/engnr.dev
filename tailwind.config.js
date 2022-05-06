eab3f8d8f5fcb9d3ef47f04fb192d20528539ad8
Ivan Aleksashin <algos.inc@gmail.com>
2022-05-06 23:51:02 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
+
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      lineHeight: {
+        snugish: "1.25",
+        normal: "2",
+      },
+      maxWidth: {
+        letter: "72rem",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+
+        black: "#24262d",
+        white: "#fdfdfd",
+
+        gray: {
+          150: "#fdfdfd",
+          250: "#24262d",
+          550: "#8b9cbe",
+          650: "#555e70",
+          750: "#343a40",
+          ...defaultTheme.colors.gray,
+        },
+        link: "#b2bfd9",
+      },
+    },
   },
   plugins: [],
 }
